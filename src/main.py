59d806cf31a1080455ecbbdbb791cc801430f4ad
Brayan Castro <davidbarbosa0525@gmail.com>
2023-05-08 20:42:36 -0500

    modificación interfaz ventanas tipo tab QTabWidget

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ month_text = {'ene.' : '01', 'feb.' : '02', 'mar.' : '03',
 
 class MainWindow(QMainWindow):
     def __init__(self):
-
         QMainWindow.__init__(self)
 
         # Carga el archivo .ui
@@ -26,27 +25,29 @@ class MainWindow(QMainWindow):
         self.calendar.selectionChanged.connect(self.change_date)
         self.hour.editingFinished.connect(self.change_time)
 
-        self.dateSelected = self.calendar.selectedDate().toString().split(' ')
+        self.dateSelected = self.dateModified(self.calendar.selectedDate().toString().split(' '))
         self.timeSelected = self.hour.time().toString()
 
 
-    def change_date(self):
-
-        self.dateSelected = self.calendar.selectedDate().toString().split(' ')
-
-        day = self.dateSelected[2]
-        month = month_text[self.dateSelected[1]]
-        age = self.dateSelected[3]
+    def dateModified(self, dateActual):
+        day = dateActual[2]
+        month = month_text[dateActual[1]]
+        age = dateActual[3]
 
         if int(day) < 10:
             day = '0' + day
 
-        self.dateSelected = day + '/' + month + '/' + age
+        dateActual = day + '/' + month + '/' + age
+
+        return dateActual
+
+
+    def change_date(self):
+        self.dateSelected = self.dateModified(self.calendar.selectedDate().toString().split(' '))
         print(self.dateSelected, self.timeSelected)
 
 
     def change_time(self):
-
         self.timeSelected = self.hour.time().toString()
         print(self.dateSelected, self.timeSelected)
         
