4ebbdf802cd0e9d0623c9ec324532c7e3d5ffcfc
Brayan Castro <davidbarbosa0525@gmail.com>
2023-05-08 23:00:28 -0500

    creación de los documentos independientes para la evidencia de cada llamada

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Evidence Formats
 ## Descripción
 
-Este proyecto consiste en agilizar el proceso de la creación de los archivos que sirven como evidencia para las llamadas que se gestionan en una empresa cualquiera. Los lenguajes que se utilizan en este proyecto son Python y su herramienta PyQt5 para facilitar la interacción del usuario junto con el gestor de base de datos SQLite.
+Este proyecto consiste en agilizar el proceso de la creación de los archivos que sirven como evidencia para las llamadas que se gestionan en una empresa. Los lenguajes que se utilizan en este proyecto son Python y su herramienta PyQt5 para facilitar la interacción del usuario junto con el gestor de base de datos SQLite.
 
 
 
@@ -16,4 +16,18 @@ Este proyecto consiste en agilizar el proceso de la creación de los archivos qu
 ``sudo apt-get install qttools5-dev-tools``
 
 
+
+## Creación de interfaz
+
+Con la herramienta Qt Designer se diseña la siguiente interfaz gráfica (HMI):
+
+![](./img/interfaz.png)
+
+![](./img/interfaz2.png)
+
+## Creación base de datos (SQLite)
+
+Para la empresa es indispensable la siguiente información: identificador del cliente, nombre, celular, dirección y la ciudad en la que reside, para almacenar esta información es necesario usar SQLite, se crea la tabla como se ve en la siguiente imagen:
+
+![Base de datos clientes](./img/table_database.png)
 
--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QCalendarWidget, QTimeEdi
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
 import sqlite3
+import csv
+
+list_clients = []
 
 month_text = {'ene.' : '01', 'feb.' : '02', 'mar.' : '03', 
               'abr.' : '04', 'may.' : '05', 'jun.' : '06',
@@ -21,15 +24,18 @@ class MainWindow(QMainWindow):
         self.calendar = self.findChild(QCalendarWidget, 'widget_calendar')
         self.hour = self.findChild(QTimeEdit, 'time_hour')
         self.client_doc = self.findChild(QLineEdit, 'client_doc')
+        self.employee_doc = self.findChild(QLineEdit, 'employee_doc')
         self.comment_doc = self.findChild(QTextEdit, 'comment_doc')
-        self.button_adjunt = self.findChild(QPushButton, 'button_adjunt')
+        self.button_add = self.findChild(QPushButton, 'button_add')
+        self.button_create_documents = self.findChild(QPushButton, 'button_create_docs')
 
         self.dialog = QMessageBox(self)
 
         # connection function when change selection calendar Widget
         self.calendar.selectionChanged.connect(self.change_date)
         self.hour.editingFinished.connect(self.change_time)
-        self.button_adjunt.clicked.connect(self.get_data_documentation)
+        self.button_add.clicked.connect(self.get_data_documentation)
+        self.button_create_documents.clicked.connect(self.create_documentation)
 
         self.dateSelected = self.dateModified(self.calendar.selectedDate().toString().split(' '))
         self.timeSelected = self.hour.time().toString()
@@ -64,6 +70,7 @@ class MainWindow(QMainWindow):
 
     def get_data_documentation(self):
         text_client_doc = self.client_doc.text()
+        text_employee_doc = self.employee_doc.text()
         text_comment_doc = self.comment_doc.toPlainText()
         
         if text_client_doc != '' and  text_comment_doc != '' and self.timeSelected != '00:00:00':
@@ -72,19 +79,34 @@ class MainWindow(QMainWindow):
                 miCursor = miConexion.cursor()
                 miCursor.execute('SELECT * FROM client WHERE id_client={}'.format(text_client_doc))
                 Query = miCursor.fetchone()
+                miConexion.close()
             except:
                 self.message_dialog('Error Conexión Base Datos')
-            miConexion.close()
-            print(Query)
 
             if(Query == None):
                 self.message_dialog('No existe el cliente')
+            else:
+                list_clients.append([Query[0], Query[1], Query[2], Query[3], Query[4], self.timeSelected + ' ' + self.dateSelected, text_employee_doc, text_comment_doc])
+                print(list_clients)
 
-
-            print(self.dateSelected, self.timeSelected, text_client_doc, text_comment_doc)
         else:
             self.message_dialog('Información Incompleta')
 
+    def create_documentation(self):
+        for item in list_clients:
+            doc = DocxTemplate("plantilla.docx")
+            context = { 'fecha' : item[5],
+                        'id_cliente' : item[0],
+                        'cliente' : item[2],
+                        'celular' : item[1],
+                        'empleado' : item[6],
+                        'direccion' : item[3],
+                        'ciudad' : item[4],
+                        'comentario' : item[7],
+                        'imagen_evidencia': InlineImage(doc, "./capture/" + str(item[0]) + ".png", width = Mm(150))
+                        }
+            doc.render(context)
+            doc.save("Evidencia ID " + str(item[0]) + " " + str(self.dateSelected).replace('/', '_') + ".docx")
 
 app = QApplication(sys.argv)
 
