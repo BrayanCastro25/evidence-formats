874b0fa3488c1f48f2df94c885207116275aac24
Brayan Castro <davidbarbosa0525@gmail.com>
2023-05-12 23:46:11 -0500

    se creo la función de eliminar el registro de un cliente

--- src/main.py
+++ src/main.py
@@ -55,10 +55,12 @@ class MainWindow(QMainWindow):
         # element to warn the user
         self.dialog = QMessageBox(self)
 
+        # date related items
         self.dateSelected = self.dateModified(self.calendar.selectedDate().toString().split(' '))
         self.timeSelected = self.hour.time().toString()
 
 
+    # function in charge of modifying the month of numerical type
     def dateModified(self, dateActual):
         day = dateActual[2]
         month = month_text[dateActual[1]]
@@ -72,6 +74,7 @@ class MainWindow(QMainWindow):
         return dateActual
 
 
+    # function in charge of removing the text from the text boxes of the editing tab
     def delete_info_edit(self):
         self.client_edit.setText("")
         self.phone_edit.setText("")
@@ -80,7 +83,7 @@ class MainWindow(QMainWindow):
         self.city_edit.setText("")
 
 
-
+    # function in charge of consulting the client's id in the database
     def consult_db(self, id):
         try: 
             miConexion = sqlite3.connect('./client', timeout = 5)
@@ -99,6 +102,7 @@ class MainWindow(QMainWindow):
             self.message_dialog('Error Conexión Base Datos')
         
 
+    # function responsible for inserting a customer in the database
     def insert_db(self):        
 
         text_client = self.client_edit.text()
@@ -121,21 +125,24 @@ class MainWindow(QMainWindow):
             print("Inf")
 
         
-
+    # function responsible for storing the date when the user modifies it
     def change_date(self):
         self.dateSelected = self.dateModified(self.calendar.selectedDate().toString().split(' '))
 
 
+    # function responsible for storing the time when the user modifies it
     def change_time(self):
         self.timeSelected = self.hour.time().toString()
 
 
+    # function responsible for notifying the user of any warnings
     def message_dialog(self, message):
         self.dialog.setWindowTitle('Advertencia')
         self.dialog.setText(message)
         self.dialog.exec()
 
 
+    # function responsible for storing the data for each of the documents in the list
     def get_data_documentation(self):
         text_client_doc = self.client_doc.text()
         text_employee_doc = self.employee_doc.text()
@@ -152,6 +159,7 @@ class MainWindow(QMainWindow):
             self.message_dialog('Información Incompleta')
 
 
+    # function responsible for creating all documents that have been registered
     def create_documentation(self):
         for item in list_clients:
             doc = DocxTemplate("plantilla.docx")
@@ -169,6 +177,7 @@ class MainWindow(QMainWindow):
             doc.save("./evidence/Evidencia ID " + str(item[0]) + " " + str(self.dateSelected).replace('/', '_') + ".docx")
 
 
+    # function responsible for obtaining and displaying customer data
     def search_id(self):
         id = self.search_edit.text()
         if id != None:
@@ -179,11 +188,13 @@ class MainWindow(QMainWindow):
             self.address_edit.setText(str(Query[3]))
             self.city_edit.setText(str(Query[4]))
 
-            
+    
+    # function responsible for creating a customer in the database
     def create_client(self):
         self.insert_db()
 
 
+    # general function that allows you to create, update and delete a client.
     def conection_db(self, sql):
         miConexion = sqlite3.connect('./client', timeout = 5)
         miCursor = miConexion.cursor()
@@ -191,6 +202,8 @@ class MainWindow(QMainWindow):
         miConexion.commit() 
         miCursor.close()  
 
+
+    # function that allows you to update a customer's data
     def update_client(self):
         text_client = self.client_edit.text()
         text_phone = self.phone_edit.text()
@@ -206,6 +219,7 @@ class MainWindow(QMainWindow):
             self.message_dialog('Error Conexión Base Datos')
 
 
+    # function to delete a customer record
     def delete_client(self):
         text_client = self.client_edit.text()
         try:
